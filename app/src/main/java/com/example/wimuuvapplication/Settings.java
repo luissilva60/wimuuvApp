c349b9bbce50d33c8a7918333faba030116b9439
RX <rockzilla.ra@gmail.com>
2021-11-29 19:22:36 +0000

    c

---
+++ app/src/main/java/com/example/wimuuvapplication/Settings.java
@@ -0,0 +1,14 @@
+package com.example.wimuuvapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Toolbar;
+
+public class Settings extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
