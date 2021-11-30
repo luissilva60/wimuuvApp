53ae1fdc6202e6a8ffee0aae9990e6dcd5aa3e6a
RX <rockzilla.ra@gmail.com>
2021-11-30 13:48:10 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/ForgotPassword.java
+++ app/src/main/java/com/example/wimuuvapplication/ForgotPassword.java
@@ -3,6 +3,10 @@ package com.example.wimuuvapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ForgotPassword extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
+
+        ImageView leftIcon = findViewById(R.id.left_icon);
+        TextView title = findViewById(R.id.toolbar_title);
+
+        leftIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(ForgotPassword.this,"You clicked in left icon",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        title.setText("Voltar");
     }
 }
