f35dc90ca27a6b8afe3d4231a4740bcb317e84de
RX <rockzilla.ra@gmail.com>
2021-12-04 19:53:43 +0000

    commit forgot password not working

--- app/src/main/java/com/example/wimuuvapplication/ForgotPassword.java
+++ app/src/main/java/com/example/wimuuvapplication/ForgotPassword.java
@@ -1,8 +1,11 @@
 package com.example.wimuuvapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,17 +17,16 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-
-        ImageView leftIcon = findViewById(R.id.left_icon);
         TextView title = findViewById(R.id.toolbar_title);
 
-        leftIcon.setOnClickListener(new View.OnClickListener() {
+        Toolbar toolbar2 = (Toolbar) findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar2);
+        toolbar2.setNavigationIcon(R.drawable.ic_baseline_arrow_back_24);
+        toolbar2.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Toast.makeText(ForgotPassword.this,"You clicked in left icon",Toast.LENGTH_SHORT).show();
+            public void onClick(View v) {
+                onBackPressed(); // Implemented by activity
             }
         });
-
-        title.setText("Voltar");
     }
 }
--- app/src/main/java/com/example/wimuuvapplication/ProfileActivity.java
+++ app/src/main/java/com/example/wimuuvapplication/ProfileActivity.java
@@ -16,24 +16,6 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        ImageView leftIcon = findViewById(R.id.left_icon);
-        ImageView rightIcon = findViewById(R.id.right_icon);
-        TextView title = findViewById(R.id.toolbar_title);
-
-        leftIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(ProfileActivity.this,"You clicked in left icon",Toast.LENGTH_SHORT).show();
-            }
-        });
-        rightIcon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(ProfileActivity.this,"You clicked in right icon",Toast.LENGTH_SHORT).show();
-            }
-        });
-
-        title.setText("Profile");
     }
 
     public void onClickMap(View v) {
