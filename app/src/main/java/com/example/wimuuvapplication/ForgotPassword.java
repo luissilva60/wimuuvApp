f2c936d6a0e3bbbdd896bde1e041be0c4c2c05cb
RX <rockzilla.ra@gmail.com>
2021-12-04 22:26:22 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/ForgotPassword.java
+++ app/src/main/java/com/example/wimuuvapplication/ForgotPassword.java
@@ -3,10 +3,12 @@ package com.example.wimuuvapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -17,11 +19,17 @@ public class ForgotPassword extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password);
-
+        Button button2 = findViewById(R.id.button2);
         Toolbar toolbar2 = (Toolbar) findViewById(R.id.toolbar2);
         toolbar2.setTitle("Forgot Password");
         setSupportActionBar(toolbar2);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public void onClickButton2(View v) {
+        Intent intent = new Intent(getApplicationContext(),Register.class);
+
+        startActivity(intent);
     }
 
     @Override
@@ -29,4 +37,6 @@ public class ForgotPassword extends AppCompatActivity {
         onBackPressed();
         return super.onSupportNavigateUp();
     }
+
+
 }
