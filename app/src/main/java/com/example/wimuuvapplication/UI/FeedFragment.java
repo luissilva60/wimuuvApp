6b4b6c3664e80edb662d203c0f534758bbc6acf0
RX <rockzilla.ra@gmail.com>
2021-12-06 23:00:32 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/FeedFragment.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/FeedFragment.java
@@ -30,14 +30,6 @@ public class FeedFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Feed.
-     */
     // TODO: Rename and change types and number of parameters
     public static FeedFragment newInstance(String param1, String param2) {
         FeedFragment fragment = new FeedFragment();
