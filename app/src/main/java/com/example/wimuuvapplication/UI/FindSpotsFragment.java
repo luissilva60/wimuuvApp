6f88157411c66aff30780186df1491125d56d0e4
RX <rockzilla.ra@gmail.com>
2021-12-07 20:56:21 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/FindSpotsFragment.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/FindSpotsFragment.java
@@ -11,7 +11,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.example.wimuuvapplication.databinding.FragmentFindEventsBinding;
 import com.example.wimuuvapplication.databinding.FragmentFindSpotsBinding;
 import com.example.wimuuvapplication.downloaders.JSONArrayDownloader;
 
