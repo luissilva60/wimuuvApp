46900f59c9c09770731903babda3306339d98737
Luis Silva <ifaf.silva12@gmail.com>
2021-12-10 20:10:15 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/OrgActivity.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/OrgActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.wimuuvapplication.R;
+
 public class OrgActivity extends AppCompatActivity {
 
     @Override
