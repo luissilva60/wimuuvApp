58ed88aabce85409cc0214af7426d48062598064
RX <rockzilla.ra@gmail.com>
2021-12-11 00:13:16 +0000

    c

---
+++ app/src/main/java/com/example/wimuuvapplication/UI/ForgotPasswordOrg.java
@@ -0,0 +1,14 @@
+package com.example.wimuuvapplication.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ForgotPasswordOrg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password_org);
+    }
+}
