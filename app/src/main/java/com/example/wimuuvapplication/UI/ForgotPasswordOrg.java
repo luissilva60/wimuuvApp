7b3aba556759717f8bff369109258e50d79d6d2a
RX <rockzilla.ra@gmail.com>
2021-12-11 00:13:21 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/ForgotPasswordOrg.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/ForgotPasswordOrg.java
@@ -1,8 +1,12 @@
 package com.example.wimuuvapplication.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.example.wimuuvapplication.R;
 
 public class ForgotPasswordOrg extends AppCompatActivity {
 
@@ -10,5 +14,14 @@ public class ForgotPasswordOrg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forgot_password_org);
+        Toolbar toolbar2 = (Toolbar) findViewById(R.id.toolbar2);
+        toolbar2.setTitle("Forgot Password Org");
+        setSupportActionBar(toolbar2);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return super.onSupportNavigateUp();
     }
 }
