6155edfaba27de22460363ec0d33865da75f7bea
Luis Silva <ifaf.silva12@gmail.com>
2021-12-13 20:05:20 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/MapsFragment.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/MapsFragment.java
@@ -140,7 +140,7 @@ public class MapsFragment extends Fragment  {
 
                 markers = new ArrayList<>();
                 for(int i = 0; i < spotlocation.size(); i++){
-                    markers.add(mMap.addMarker(new MarkerOptions().position(spotlocation.get(i)).title(spotName.get(i)).icon
+                    markers.add(googleMap.addMarker(new MarkerOptions().position(spotlocation.get(i)).title(spotName.get(i)).icon
                             (BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA))));
                 }
                 ///
