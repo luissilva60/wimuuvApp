6c00017114c2ff6e5a0d9d0970947ca046514824
Luís Silva <ifaf.silva12@gmail.com>
2021-12-16 13:19:43 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/SpotDetailsActivity.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/SpotDetailsActivity.java
@@ -3,6 +3,7 @@ package com.example.wimuuvapplication.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.wimuuvapplication.R;
 
@@ -12,6 +13,9 @@ public class SpotDetailsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Bundle bundle = getIntent().getExtras();
+        int id = bundle.getInt("id");
+        Log.e("asdasdasdasdas", "Spot id in spot details: " + id);
         setContentView(R.layout.activity_spot_details);
     }
 }
