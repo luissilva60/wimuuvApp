231a7c6a58b2408f19d81ca44cefe3bb05401929
Luis Silva <ifaf.silva12@gmail.com>
2021-12-20 13:58:29 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/Org/FeedDetailsOrg.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/Org/FeedDetailsOrg.java
@@ -75,7 +75,7 @@ public class FeedDetailsOrg extends AppCompatActivity {
         qrcode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), QRCodeEventActivity.class);
+                Intent intent = new Intent(getApplicationContext(), ReadQRCodeActivity.class);
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/wimuuvapplication/UI/Org/ReadQRCodeActivity.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/Org/ReadQRCodeActivity.java
@@ -1,16 +1,105 @@
 package com.example.wimuuvapplication.UI.Org;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import eu.livotov.labs.android.camview.ScannerLiveView;
+import eu.livotov.labs.android.camview.scanner.decoder.zxing.ZXDecoder;
+
+import static android.Manifest.permission.CAMERA;
+import static android.Manifest.permission.VIBRATE;
 
 import com.example.wimuuvapplication.R;
 
 public class ReadQRCodeActivity extends AppCompatActivity {
+    private ScannerLiveView camera;
+    private TextView scanned;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_read_qrcode);
+
+        if (checkPermission()) {
+            Toast.makeText(this, "Permission Granted..", Toast.LENGTH_SHORT).show();
+        } else {
+            requestPermission();
+        }
+
+        scanned = findViewById(R.id.dadosScanner);
+        camera = (ScannerLiveView) findViewById(R.id.camview);
+
+        camera.setScannerViewEventListener(new ScannerLiveView.ScannerViewEventListener() {
+            @Override
+            public void onScannerStarted(ScannerLiveView scanner) {
+                Toast.makeText(ReadQRCodeActivity.this, "A Iniciar Scan", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onScannerStopped(ScannerLiveView scanner) {
+                Toast.makeText(ReadQRCodeActivity.this, "A parar Scan", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onScannerError(Throwable err) {
+                Toast.makeText(ReadQRCodeActivity.this, "Erro de Scan: " + err.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onCodeScanned(String data) {
+                scanned.setText(data);
+            }
+        });
+    }
+
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        ZXDecoder decoder = new ZXDecoder();
+        // 0.5 is the area where we have
+        // to place red marker for scanning.
+        decoder.setScanAreaPercent(0.8);
+        camera.setDecoder(decoder);
+        camera.startScanner();
+    }
+
+    @Override
+    protected void onPause() {
+        camera.stopScanner();
+        super.onPause();
+    }
+
+    private boolean checkPermission() {
+        int camera_permission = ContextCompat.checkSelfPermission(getApplicationContext(), CAMERA);
+        int vibrate_permission = ContextCompat.checkSelfPermission(getApplicationContext(), VIBRATE);
+        return camera_permission == PackageManager.PERMISSION_GRANTED && vibrate_permission == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestPermission() {
+        int PERMISSION_REQUEST_CODE = 200;
+        ActivityCompat.requestPermissions(this, new String[]{CAMERA, VIBRATE}, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length > 0) {
+            boolean cameraaccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED;
+            boolean vibrateaccepted = grantResults[1] == PackageManager.PERMISSION_GRANTED;
+            if (cameraaccepted && vibrateaccepted) {
+                Toast.makeText(this, "Permission granted..", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Permission Denined \n You cannot use app without providing permission", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
