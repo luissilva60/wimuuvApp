c309ed45a94c0cc0e4ebd2c3b5535ac94beb08ef
Luis Silva <ifaf.silva12@gmail.com>
2021-12-21 18:49:26 +0000

    c

--- app/src/main/java/com/example/wimuuvapplication/UI/Org/SettingsOrgActivity.java
+++ app/src/main/java/com/example/wimuuvapplication/UI/Org/SettingsOrgActivity.java
@@ -2,15 +2,28 @@ package com.example.wimuuvapplication.UI.Org;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.wimuuvapplication.R;
 
 public class SettingsOrgActivity extends AppCompatActivity {
+    private Button logout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settingsorg);
+
+        logout = findViewById(R.id.logoutBtnOrg);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), OrgLoginActivity.class);
+            }
+        });
     }
 }
